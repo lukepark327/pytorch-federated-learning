1c100c8afce40c54a7ae1d13b31b0ea8921ea0e8
Luke Park <lukepark327@gmail.com>
2019-11-17 15:44:22 +0900

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -2,12 +2,12 @@
 # blockchain-like shared database for simulation.
 # Not real blockchain structures and functions.
 from flmodel import Flmodel
+from utils import MetaFunc
 
 
-# DAG
 class Reference:
-    def __init__(self, transaction, metrics: list = list()):
-        self.transaction = transaction
+    def __init__(self, id: str, metrics: list = list()):
+        self.id = id
         self.metrics = metrics  # loss, acc, et al.
 
 
@@ -17,68 +17,148 @@ class Transaction:
             id: str,
             prev_transactions: list = list(),
             prev_transactions_metrics: list = list(),
-            flmodel: Flmodel = None):
+            flmodel: Flmodel = None,
+            weight: int = 1):
         self.id = id
         self.flmodel = flmodel
-        self.weights = 0  # weight using DAG's tx
+        self.weight = weight  # weight using DAG's tx
         self.references = self.__set_references(
             prev_transactions, prev_transactions_metrics)
-        self.__update_predecessors_weights()
 
     def __set_references(self, prev_transactions: list, prev_transactions_metrics: list):
         if len(prev_transactions) != len(prev_transactions_metrics):
             raise ValueError
 
         res = list()
-        for prev_tx, prev_tx_metrics in zip(prev_transactions, prev_transactions_metrics):
-            res.append(Reference(prev_tx, prev_tx_metrics))
+        for prev_tx_id, prev_tx_metrics in zip(prev_transactions, prev_transactions_metrics):
+            res.append(Reference(prev_tx_id, prev_tx_metrics))
         return res
 
-    def get_all_predecessors(self):
-        predecessors = set()
-        for refer in self.references:
-            predecessors.add(refer.transaction)
-            grands = refer.transaction.get_all_predecessors()
-            predecessors.union(grands)
-        return predecessors
-
-    # TODO: policy
-    def __update_predecessors_weights(self):
-        predecessors = self.get_all_predecessors()
-        for predecessor in predecessors:
-            predecessor.weights += 1
+    def get_references_ids(self):
+        return [r.id for r in self.references]
 
 
 class Blockchain:
     def __init__(
+            self,
+            genesis_transaction: Transaction,
+            policy_update_txs_weight_name: str = None,
+            policy_update_txs_weight_func: callable = None,
+            policy_snapshot_name: str = None,
+            policy_snapshot_func: callable = None,
+            *args):
+        self.genesis_transaction = genesis_transaction
+        self.__policy_update_txs_weight = MetaFunc(
+            policy_update_txs_weight_name,
+            policy_update_txs_weight_func
+        )
+        self.__policy_snapshot = MetaFunc(
+            policy_snapshot_name,
+            policy_snapshot_func
+        )
+        self.transactions = dict()
+        self.add_transaction(genesis_transaction)
 
-    ):
-        pass
+    def print(self):
+        print("")
+        print("tx_num    :\t", len(self.transactions.keys()))
+        print("tx_weights:\t", {
+              tx.id: tx.weight for tx in self.transactions.values()})
+        print("policies  :\t", "update_txs_weight: ",
+              self.get_policy_update_txs_weight_name())
+        print("policies  :\t", "snapshot: ",
+              self.get_policy_snapshot_name())
+
+    """transaction"""
+
+    def get_transaction_by_id(self, id: str):
+        return self.transactions[id]
+
+    def get_all_predecessors_by_id(self, id: str):
+        references_id = self.get_transaction_by_id(id).get_references_ids()
+
+        predecessors = set()
+        for r_id in references_id:
+            predecessors.add(r_id)
+            grands = self.get_all_predecessors_by_id(r_id)
+            predecessors = predecessors.union(grands)
+        return predecessors
+
+    def add_transaction(self, tx: Transaction, *args):
+        # invalid tx cases
+        if tx.id in self.transactions:
+            raise ValueError
+        predecessors_ids = tx.get_references_ids()
+        for p_id in predecessors_ids:
+            if p_id not in self.transactions:
+                raise ValueError
+
+        # add transaction in blockchain
+        self.transactions[tx.id] = tx
+
+        # update predecessors' weight
+        self.update_txs_weight(self, tx.id, *args)
+
+        # TODO: snapshot if needed
+        # self.snapshot()  # TODO: *args...?
+
+    """policies"""
+
+    def update_txs_weight(self, id: str, *args):
+        return self.__policy_update_txs_weight.func(id, *args)
+
+    def snapshot(self, *args):
+        return self.__policy_snapshot.func(*args)
+
+    def get_policy_update_txs_weight_name(self):
+        return self.__policy_update_txs_weight.name
+
+    def get_policy_snapshot_name(self):
+        return self.__policy_snapshot.name
+
+    def set_policy_update_txs_weight(
+            self,
+            policy_update_txs_weight_name,
+            policy_update_txs_weight_func):
+        self.__policy_update_txs_weight = MetaFunc(
+            policy_update_txs_weight_name,
+            policy_update_txs_weight_func
+        )
+
+    def set_policy_snapshot(
+            self,
+            policy_snapshot_name,
+            policy_snapshot_func):
+        self.__policy_snapshot = MetaFunc(
+            policy_snapshot_name,
+            policy_snapshot_func
+        )
 
 
 if __name__ == "__main__":
-    # Genesis Tx
-    tx_0 = Transaction(str(0))
-    print("0: ", tx_0.weights, [t.id for t in tx_0.get_all_predecessors()])
-    print("=" * 8)
+    def my_policy_update_txs_weight(self: Blockchain, id: str):
+        amount = self.get_transaction_by_id(id).weight
+        predecessors_ids = self.get_all_predecessors_by_id(id)
+        for p_id in predecessors_ids:
+            p_tx = self.get_transaction_by_id(p_id)
+            p_tx.weight += amount
 
-    # Tx_1
-    tx_1 = Transaction(str(1), [tx_0], [0.4])
-    print("0: ", tx_0.weights, [t.id for t in tx_0.get_all_predecessors()])
-    print("1: ", tx_1.weights, [t.id for t in tx_1.get_all_predecessors()])
-    print("=" * 8)
+    genesis_transaction = Transaction("0")
+    blockchain = Blockchain(
+        genesis_transaction,
+        policy_update_txs_weight_name="heaviest",
+        policy_update_txs_weight_func=my_policy_update_txs_weight)
 
-    # Tx_2
-    tx_2 = Transaction(str(2), [tx_0, tx_1], [0.2, 0.0])
-    print("0: ", tx_0.weights, [t.id for t in tx_0.get_all_predecessors()])
-    print("1: ", tx_1.weights, [t.id for t in tx_1.get_all_predecessors()])
-    print("2: ", tx_2.weights, [t.id for t in tx_2.get_all_predecessors()])
-    print("=" * 8)
+    blockchain.print()
 
-    # Tx_3
-    tx_3 = Transaction(str(3), [tx_1, tx_2], [0.8, 0.1])
-    print("0: ", tx_0.weights, [t.id for t in tx_0.get_all_predecessors()])
-    print("1: ", tx_1.weights, [t.id for t in tx_1.get_all_predecessors()])
-    print("2: ", tx_2.weights, [t.id for t in tx_2.get_all_predecessors()])
-    print("3: ", tx_3.weights, [t.id for t in tx_3.get_all_predecessors()])
-    print("=" * 8)
+    tx_1 = Transaction("1", ["0"], [0.8])
+    blockchain.add_transaction(tx_1)
+    blockchain.print()
+
+    tx_2 = Transaction("2", ["0", "1"], [0.9, 0.7])
+    blockchain.add_transaction(tx_2)
+    blockchain.print()
+
+    tx_3 = Transaction("3", ["2"], [0.3])
+    blockchain.add_transaction(tx_3)
+    blockchain.print()
