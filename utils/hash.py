ce4ffd7e2e13a716f0fabcd6ced378b1c6c9acc8
Chan Park <cpark995@gmail.com>
2019-12-13 16:20:57 +0900

    Make project structure

---
+++ utils/hash.py
@@ -0,0 +1,23 @@
+import hashlib
+import numpy as np
+
+class MetaFunc:
+    def __init__(self, name=None, func=None):
+        self.name = name
+        self.func = func
+
+
+def sha3_256_from_array(inputs=[]):
+    SHA3 = hashlib.sha3_256()
+    bytes_array = [np.array(inputs).tobytes()]
+    for i in bytes_array:
+        SHA3.update(i)
+    return SHA3.hexdigest()
+
+
+def cal_weights_hash(weights: list):
+    weights_list = list()
+    for weight in weights:
+        b = weight.tobytes()
+        weights_list.append(b)
+    return sha3_256(weights_list)
