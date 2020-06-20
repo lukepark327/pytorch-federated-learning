a54d869c8e86f641874626e252f81da4d9344ab8
lukepark327 <lukepark327@gmail.com>
2020-06-20 21:15:45 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 python src/main.py
-# python src/main.py --nNodes=10 --nEpochs=10
-# python src/main.py --nNodes=10 --nByzs=3 --path="byz_acc"
+# python src/main.py --nNodes=10 --nByzs=3 --path="byz_acc" --repute="acc"
+# python src/main.py --nNodes=10 --nByzs=3 --path="byz_Frobenius_FN_OS" --repute="Frobenius" --op-stop --filter
